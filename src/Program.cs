631f9ac119124eda328fa7012091e248892ed0a7
Kurris <791444095@qq.com>
2022-08-09 11:00:31 +0800

    fix serilog

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
 
 
 namespace IdentityServer.STS.Admin
@@ -17,22 +16,7 @@ namespace IdentityServer.STS.Admin
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-                .UseSerilog((context, configuration) =>
-                {
-                    configuration.MinimumLevel.Debug()
-                        .MinimumLevel.Override("System", LogEventLevel.Warning)
-                        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                        .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
-                        .MinimumLevel.Override("IdentityServer4", LogEventLevel.Information)
-                        .MinimumLevel.Override("AspNet.Security.OAuth", LogEventLevel.Information)
-#if DEBUG
-                        .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Information)
-#else
-                        .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command",LogEventLevel.Warning)
-#endif
-                        .Enrich.FromLogContext()
-                        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}"
-                            , theme: SystemConsoleTheme.Literate);
-                });
+                .ConfigureLogging((_, builder) => builder.ClearProviders())
+                .UseSerilog((context, configuration) => { configuration.ReadFrom.Configuration(context.Configuration); });
     }
 }
